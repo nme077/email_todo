77270c57ae270804434eb21784153537b3d23227
Nicholas Eveland <nmemusic77@gmail.com>
2021-06-23 10:35:21 -0400

    Delete a route

--- server/index.js
+++ server/index.js
@@ -30,9 +30,6 @@ app.use(session({
 const oAuth2Client = new google.auth.OAuth2(process.env.CLIENT_ID, process.env.CLIENT_SECRET, process.env.REDIRECT_URI);
 oAuth2Client.setCredentials({refresh_token: process.env.REFRESH_TOKEN});
 
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
-  });
 
 app.post('/send', (req,res) => {
     async.waterfall([
