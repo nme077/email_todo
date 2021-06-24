84fbb35fa3961fe98f54782efb3535280264d6a4
Nicholas Eveland <nmemusic77@gmail.com>
2021-06-24 19:26:04 -0400

    Fix opacity bug

--- client/src/App.js
+++ client/src/App.js
@@ -23,7 +23,7 @@ class SuccessScreen extends React.Component {
   render() {
     return(
       <div>
-        <div className="text-center success d-flex align-items-center justify-content-center"></div>
+        <div className="text-center loadingScreen d-flex align-items-center justify-content-center"></div>
         <div className="position-absolute d-flex align-items-center justify-content-center w-100 h-100 p-0 statusIcon">
           <svg xmlns="http://www.w3.org/2000/svg" width="90" height="90" fill="#99ff00" className="bi bi-check-lg bounce" viewBox="0 0 16 16">
             <path d="M13.485 1.431a1.473 1.473 0 0 1 2.104 2.062l-7.84 9.801a1.473 1.473 0 0 1-2.12.04L.431 8.138a1.473 1.473 0 0 1 2.084-2.083l4.111 4.112 6.82-8.69a.486.486 0 0 1 .04-.045z"/>
