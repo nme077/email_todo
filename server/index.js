5ac0112e19c30eed40e0d61c6956da06a71737cf
Nicholas Eveland <nmemusic77@gmail.com>
2021-06-24 17:20:36 -0400

    Move postscript to server

--- server/index.js
+++ server/index.js
@@ -84,7 +84,7 @@ app.post('/send', (req,res) => {
 if(process.env.NODE_ENV === 'production') {  
     app.use(express.static(path.join(__dirname, 'client/build'))); 
     app.get('*', (req, res) => {    
-        res.sendFile(path.join(__dirname, 'client/build/index.html'));  
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));  
     })
 }
   
